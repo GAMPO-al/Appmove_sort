ce88c8fea543fffddf9006f2735ca4040b69a0dc
Static-Name <64046097+GAMPO-al@users.noreply.github.com>
2021-04-05 21:45:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+'''def sorting(liste,n):	
+	Max  = max(liste)
+	new_list = []
+	new_list.append(Max)
+	if len(new_list)!=4:
+		return liste[]
+	else:
+		return sorting(liste[:n],n-1)'''
+
+def Appmove_sort(liste):
+	new_list = []
+	for i in range(len(liste)):
+		Min = min(liste)
+		new_list.append(Min)
+		liste.remove(Min)
+	return new_list
+
+
+
+liste = [19,44,65,26,54,22,59,11,3,323,77,854,23,2123,5,767,997,11]
+
+print("Unsorted list {}".format(liste))
+print("sorted list {}\t".format(Appmove_sort(liste)))
